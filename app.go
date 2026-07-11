829c460486da2cf6ab3d87364a4a4c844d003540
苦瓜 <1348078212@qq.com>
2026-07-11 14:46:02 +0800

    v1.7.3版本号更新

--- app.go
+++ app.go
@@ -24,7 +24,7 @@ const (
 	steamAppID  = "881020"
 	gameExeName = "granblue_fantasy_relink.exe"
 	gameFolder  = "Granblue Fantasy Relink"
-	appVersion  = "v1.7.2"
+	appVersion  = "v1.7.3"
 	repoOwner   = "BitterG"
 	repoName    = "GBFR-PE-Patch-Tool"
 )
